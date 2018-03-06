171142ba6a64b0d2d5603c6c303228d3c8c1e1d2
Aron <aron.rogmann@gmx.de>
2018-03-06 17:17:20 +0100

    Grundgerüst für Shiny App für Allgemeine Aufgaben hinzugefügt
    Grundgerüst für Shiny App für Allgemeine Aufgaben hinzugefügt und kleine Änderungen bei der Erstellung des Datensatzes vorgenommen

--- Case_Study_Allgemeine_Aufgaben.R
+++ Case_Study_Allgemeine_Aufgaben.R
@@ -8,6 +8,11 @@ logistikverzug_k7 <- read.csv2("Logistikverzug_K7.csv", header = TRUE, stringsAs
 
 # zusammenfügen der beiden Datensätze
 logistikverzug_combined_df <- left_join(komponente_k7, logistikverzug_k7, by = "IDNummer")
+# bei doppelt vorkommenden Spalten benennt R die Spalte des ersten Datasets mit .x und die Spalte mit gleichem Namen des zweiten Datasets mit .y
+
+# mit dem Befehl str() können wir einen Überblick über den gejointen Datensatz erhalten
+str(logistikverzug_combined_df)
+# der so gejointe Datensatz enthält 11 Merkmale (Spalten) mit 306490 Zeilen (observations)
 
 # löschen der doppelten Spalten 
 logistikverzug_selected_df <-  select(logistikverzug_combined_df, 1:6, 8)
@@ -20,11 +25,66 @@ names(logistikverzug_selected_df) <- column_names
 logistikverzug_selected_df$Produktionsdatum <- as.POSIXct(logistikverzug_selected_df$Produktionsdatum, "", "%Y-%m-%d")
 logistikverzug_selected_df$Wareneingang <- as.POSIXct(logistikverzug_selected_df$Wareneingang, "", "%d.%m.%Y")
 
-# erstellen einer neuen Spalte mit den Werten des Logistikverzugs in Tagen
-logistikverzug_mutated_df <- mutate(logistikverzug_selected_df, Logistikverzug = as.double(Wareneingang - Produktionsdatum)) 
+# erstellen einer neuen Spalte mit der Lieferzeit in Tagen
+logistikverzug_mutated_df <- mutate(logistikverzug_selected_df, Lieferzeit = as.double(Wareneingang - Produktionsdatum))
 
-# der minimale und maximale Logistikverzug lässt sich aus der summary für den Datensatz ablesen
-summary(logistikverzug_mutated_df)
+# Bestimmung der minimalen und maximalen Lieferzeit
+min(logistikverzug_mutated_df$Lieferzeit)
+max(logistikverzug_mutated_df$Lieferzeit)
 
-# Testplot
-hist(logistikverzug_mutated_df$Logistikverzug)
+# AUfteilung der Spalte "IDNummer" in mehrere Spalten mit int Datentypen (möglicherweise nicht nötig)
+logistikverzug_separated_df <- separate(logistikverzug_mutated_df, col=IDNummer, into = c("ID_1", "ID_2", "ID_3", "ID_4"), sep = "-")
+logistikverzug_transformed_df <- transform(logistikverzug_separated_df, ID_2 = as.numeric(ID_2),ID_3 = as.numeric(ID_3), ID_4 = as.numeric(ID_4))
+
+# Testplot (Histogram: ..density.. gibt normierte y-Achse aus)
+ggplot(logistikverzug_transformed_df, aes(x = Lieferzeit)) +
+  geom_histogram(binwidth = 1, aes(y = ..density..))
+
+
+
+
+
+# Beginn der Shiny Applikation
+library(shiny)
+
+ui <- fluidPage(
+# Überschrift hinzufügen
+  headerPanel(title = "Allgemeine Aufgaben"),
+  
+# Erstellung der Sidebar
+  sidebarLayout(
+    sidebarPanel(
+# Implementierung eines Blocks zur Zeitraumwahl
+      dateRangeInput(
+        inputId = "date",
+        label = "Zeitraum",
+        min = min(logistikverzug_mutated_df$Produktionsdatum), # frühestes auswählbares Datum
+        max = max(logistikverzug_mutated_df$Wareneingang), # spätestes auswählbares Datum
+        start = min(logistikverzug_mutated_df$Produktionsdatum), # vorausgewähltes Startdatum
+        end = max(logistikverzug_mutated_df$Wareneingang) # vorausgewähltes Enddatum
+      )
+    ),
+
+# Erstellung des Main-Panels mit zwei verschiedenen Reitern     
+    mainPanel(
+      tabsetPanel(
+# Erstellung des ersten Reiters
+        tabPanel(
+          title = "Produktionsmenge",
+          div("Balkendiagramm: Produktionsmenge") # Platzhalter für Diagramm
+        ),
+# Erstellung des zweiten Reiters
+        tabPanel(
+          title = "Karosserien in Auslieferung", 
+          div("Balkendiagramm: Karosserien in Auslieferung") # Platzhalter für Diagramm
+        )      
+      )
+    )
+  )
+)
+
+server <- function(session, input, output) {
+  
+}
+
+shinyApp(ui = ui, server = server)
