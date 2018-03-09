c94ca702d4e388fc0d1c59f23ae71f0bb2e8627a
Aron <aron.rogmann@gmx.de>
2018-03-09 17:51:04 +0100

    Änderungen an der Shiny App durchgeführt
    Erstellung des Plots im ersten Reiter begonnen

--- Case_Study_Allgemeine_Aufgaben.R
+++ Case_Study_Allgemeine_Aufgaben.R
@@ -11,7 +11,7 @@ logistikverzug_combined_df <- left_join(komponente_k7, logistikverzug_k7, by = "
 # bei doppelt vorkommenden Spalten benennt R die Spalte des ersten Datasets mit .x und die Spalte mit gleichem Namen des zweiten Datasets mit .y
 
 # mit dem Befehl str() können wir einen Überblick über den gejointen Datensatz erhalten
-str(logistikverzug_combined_df)
+# str(logistikverzug_combined_df)
 # der so gejointe Datensatz enthält 11 Merkmale (Spalten) mit 306490 Zeilen (observations)
 
 # löschen der doppelten Spalten 
@@ -36,10 +36,15 @@ max(logistikverzug_mutated_df$Lieferzeit)
 logistikverzug_separated_df <- separate(logistikverzug_mutated_df, col=IDNummer, into = c("ID_1", "ID_2", "ID_3", "ID_4"), sep = "-")
 logistikverzug_transformed_df <- transform(logistikverzug_separated_df, ID_2 = as.numeric(ID_2),ID_3 = as.numeric(ID_3), ID_4 = as.numeric(ID_4))
 
-# Testplot (Histogram: ..density.. gibt normierte y-Achse aus)
-ggplot(logistikverzug_transformed_df, aes(x = Lieferzeit)) +
-  geom_histogram(binwidth = 1, aes(y = ..density..))
+# # Testplot (Histogram: ..density.. gibt normierte y-Achse aus)
+# ggplot(logistikverzug_transformed_df, aes(x = Lieferzeit)) +
+#   geom_histogram(binwidth = 1, aes(y = ..density..))
 
+# Testplot zur Erstellung eines Verteilungsmodells
+# lin_mod <- lm( Lieferzeit ~ Produktionsdatum, data = logistikverzug_mutated_df)
+# 
+# ggplot(logistikverzug_mutated_df, aes(x = Produktionsdatum, y = Lieferzeit, col = as.factor(Werksnummer))) +
+#   geom_point(alpha = 0.2) 
 
 
 
@@ -50,13 +55,14 @@ library(shiny)
 ui <- fluidPage(
 # Überschrift hinzufügen
   headerPanel(title = "Allgemeine Aufgaben"),
-  
-# Erstellung der Sidebar
+
+# Erstellung des SidebarLayouts
   sidebarLayout(
+# Erstellung eines SidebarPanels
     sidebarPanel(
 # Implementierung eines Blocks zur Zeitraumwahl
       dateRangeInput(
-        inputId = "date",
+        inputId = "date_range",
         label = "Zeitraum",
         min = min(logistikverzug_mutated_df$Produktionsdatum), # frühestes auswählbares Datum
         max = max(logistikverzug_mutated_df$Wareneingang), # spätestes auswählbares Datum
@@ -65,26 +71,42 @@ ui <- fluidPage(
       )
     ),
 
-# Erstellung des Main-Panels mit zwei verschiedenen Reitern     
+# Erstellung des Main-Panels mit zwei verschiedenen Reitern
     mainPanel(
       tabsetPanel(
 # Erstellung des ersten Reiters
         tabPanel(
           title = "Produktionsmenge",
-          div("Balkendiagramm: Produktionsmenge") # Platzhalter für Diagramm
+          # div("Balkendiagramm: Produktionsmenge") # Platzhalter für Diagramm
+          plotOutput(
+            # An dieser Stelle wird output$plot dargestellt
+            outputId = "plot_produktionsmenge"
+          )
         ),
 # Erstellung des zweiten Reiters
         tabPanel(
-          title = "Karosserien in Auslieferung", 
+          title = "Karosserien in Auslieferung",
           div("Balkendiagramm: Karosserien in Auslieferung") # Platzhalter für Diagramm
-        )      
+        )
       )
     )
   )
 )
 
 server <- function(session, input, output) {
-  
+
+  zeitraum_df <-  reactive({
+    startdatum <- input$date_range[[1]]
+    enddatum <- input$date_range[[2]]
+    filter(logistikverzug_mutated_df, logistikverzug_mutated_df$Produktionsdatum <= enddatum && logistikverzug_mutated_df$Produktionsdatum >= startdatum)
+  })
+
+# Funktion zur Erzeugung des Diagramms im ersten Reiter
+  output$plot_produktionsmenge <- renderPlot({
+    ggplot(zeitraum_df(), aes(x = Produktionsdatum, y = Lieferzeit, col = as.factor(Werksnummer))) +
+        geom_point(alpha = 0.2)    # nicht finales Diagramm
+  })
+
 }
 
 shinyApp(ui = ui, server = server)
